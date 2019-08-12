3fc3dba33ae3a80b1add50b21a5230c3850fddd4
Shreeram <urshree@gmail.com>
2019-08-12 23:00:59 +0530

    Added Publish method to RabbitMqBus

---
+++ Microhard.Infra.Bus/RabbitMqBus.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using MicroHard.Domain.Core.Bus;
+using MicroHard.Domain.Core.Commands;
+using MicroHard.Domain.Core.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microhard.Infra.Bus
+{
+    public sealed class RabbitMqBus : IEventBus
+    {
+        private readonly IMediator _mediator;
+        private readonly Dictionary<string, List<Type>> _handlers;
+        private readonly List<Type> _eventTypes;
+        public RabbitMqBus(IMediator mediator )
+        {
+            _mediator = mediator;
+            _handlers = new Dictionary<string, List<Type>>();
+            _eventTypes = new List<Type>();
+        }
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public void Publish<T>(T @event) where T : Event
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            {
+               using (var channel = connection.CreateModel())
+                {
+                    var eventName = @event.GetType().Name;
+
+                    channel.QueueDeclare(eventName, false, false, false, null);
+
+                    var message = JsonConvert.SerializeObject(@event);
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish("", eventName, null, body);
+
+                }
+            }
+        } 
+
+        public void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
