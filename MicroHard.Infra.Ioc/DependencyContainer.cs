fa845ffcfb3444c9522af31f6cd656affcb63a0f
Shreeram <urshree@gmail.com>
2019-08-13 17:35:38 +0530

    Added DI Class

---
+++ MicroHard.Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using Microhard.Infra.Bus;
+using MicroHard.Domain.Core.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroHard.Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMqBus>();
+        }
+    }
+}
