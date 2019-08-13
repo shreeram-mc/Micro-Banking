c306ea6e4ad7fd3615f4a15506ff0a96c4ef7c8e
Shreeram <urshree@gmail.com>
2019-08-13 22:31:57 +0530

    Added Banking Controllers, Migrations. Partiall Completed Banking Get API

--- MicroHard.Infra.Ioc/DependencyContainer.cs
+++ MicroHard.Infra.Ioc/DependencyContainer.cs
@@ -1,4 +1,9 @@
 ﻿using Microhard.Infra.Bus;
+using MicroHard.Banking.Application.Interfaces;
+using MicroHard.Banking.Application.Services;
+using MicroHard.Banking.Data.Context;
+using MicroHard.Banking.Data.Repository;
+using MicroHard.Banking.Domain.Interfaces;
 using MicroHard.Domain.Core.Bus;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -11,7 +16,16 @@ namespace MicroHard.Infra.Ioc
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            //Domain Bus
             services.AddTransient<IEventBus, RabbitMqBus>();
+
+            //Application Services
+            services.AddTransient<IAccountService, AccountService>();
+
+            //Data
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<BankingDbContext>();
+
         }
     }
 }
